9dd5fba71cc156cbb533b26bab45a4bddaca417d
tom-molander <thomascolemolander@outlook.com>
2023-12-12 19:13:42 +1000

    Contacts Library Function intial set up

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 
-
+print ("Contact Library")
 
 def contacts_library():
-    pass
+    print('Enter "A" to add a new contact to your library')
+    print('Enter "R" to remove a contact from your library')
+    print('Enter "V" to view your contacts library')
+    print('Enter "E" to edit a contact')
+    print('Enter "C" to close')
 
+    input("Please enter your selection: ")
+
+contacts_library()
