754d2602fdc48e0062dfec99af56e242b35ff8b1
tom-molander <thomascolemolander@outlook.com>
2023-12-12 19:30:43 +1000

    Conditional while loop initial set up for contact library

--- main.py
+++ main.py
@@ -3,11 +3,34 @@ print ("Contact Library")
 
 def contacts_library():
     print('Enter "A" to add a new contact to your library')
-    print('Enter "R" to remove a contact from your library')
+    print('Enter "D" to delete a contact from your library')
     print('Enter "V" to view your contacts library')
     print('Enter "E" to edit a contact')
     print('Enter "C" to close')
 
-    input("Please enter your selection: ")
+    selection = input("Please enter your selection: ")
+    return selection
 
-contacts_library()
+
+users_selection = ""
+
+while users_selection != "C":
+    users_selection = contacts_library()
+    if (users_selection == "A"):
+        print ("New Contact")
+        print (input("Name: "))
+        print (input("Surname: "))
+        print (input("Phone: "))
+        print (input("Email: "))
+    elif (users_selection == "D"):
+        print("Hello World")
+    elif (users_selection == "V"):
+        print("Here is your Contacts Library")
+    elif (users_selection == "E"):
+        print("Enter contact name you want to edit")
+    elif (users_selection =="C"):
+        break
+    else: 
+        print ("Sorry I don't recognise that command, can you try again?")
+
+        
