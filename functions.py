ddb467f75368f5785d6e250a0368edbd24f8d0c4
tom-molander <thomascolemolander@outlook.com>
2023-12-15 14:39:45 +1000

    add contact function defined

--- functions.py
+++ functions.py
@@ -1,2 +1,13 @@
 import csv
 
+def add_contact(file_name):
+    print("Create new contact: ")
+
+    contact_first_name = input("First Name: ")
+    contact_surname = input("Surname: ")
+    contact_phone = input("Phone: ")
+    contact_email = input("Email: ")
+
+    with open(file_name, "a") as f:
+        writer = csv.writer(f)
+        writer.writerow([contact_first_name, contact_surname, contact_phone, contact_email])
