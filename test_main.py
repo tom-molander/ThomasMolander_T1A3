fc0401e463b52906b71a14383679016a12da0769
tom-molander <thomascolemolander@outlook.com>
2023-12-20 20:00:17 +1000

    test_main.py, add contact test

--- test_main.py
+++ test_main.py
@@ -1,3 +1,21 @@
+import csv
+from functions import add_contact
+from colored import fg, attr, bg
 
-def test_add_contact():
-    pass
+
+test_file_name = "test_contacts_library.csv"
+
+
+def test_add(monkeypatch):
+   original_length = 0
+   with open(test_file_name) as f:
+       reader = csv.reader(f)
+       original_length = sum(1 for row in reader)
+   monkeypatch.setattr('builtins.input', lambda _: "Test Add Contact")
+   add_contact(test_file_name)
+   with open(test_file_name) as f:
+       reader = csv.reader(f)
+       new_length = sum(1 for row in reader)
+   print(original_length)
+   print(new_length)
+   assert new_length == original_length + 1
